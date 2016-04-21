d861eea1bcb93b5a3b65c12b43a82d0723a141ff
OHolvik <oscar.holvik@hotmail.com>
2016-04-21 11:58:19 +0200

    .

--- TDD test/UnitTestProject1/UnitTest1.cs
+++ TDD test/UnitTestProject1/UnitTest1.cs
@@ -12,76 +12,54 @@ namespace UnitTestProject1
         {
 
             //Arrange
-            string str = "Length";
-            int expected = 6;
+            string[] str = {"Length"};
+            string expected = "6";
 
             stringUtil su = new stringUtil();
 
+
             //Act
-            int result = su.strCount(str);
+            string result = su.strCount(str);
+
 
             //Assert
+
             Assert.AreEqual(expected, result);
         }
 
         [TestMethod]
-        public void TestStrType1()
+        public void TestStrType()
         {
             //Arrange
-            string str = "hej";
-            string expected = "word";
+            string [] str = { "hej", "123", "hej123"};
+            string [] expected = { "word", "number", "none"};
 
-            stringUtil su = new stringUtil();
+             stringUtil su = new stringUtil();
             //Act
             string result = su.strType(str);
 
-            //Assert
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TestStrType2()
-        {
-            //Arrange
-            string str = "123";
-            string expected = "number";
-
-            stringUtil su = new stringUtil();
-            //Act
-            string result = su.strType(str);
 
             //Assert
-            Assert.AreEqual(expected, result);
+            Assert.Equals(expected, result);
+
+
+
         }
-
-        [TestMethod]
-        public void TestStrType3()
-        {
-            //Arrange
-            string str = "hej123";
-            string expected = "none";
-
-            stringUtil su = new stringUtil();
-            //Act
-            string result = su.strType(str);
-
-            //Assert
-            Assert.AreEqual(expected, result);
-        }
-
         [TestMethod]
         public void TestNextPalindrome()
         {
             //Arrange
-            int num = 123;
+            int [] str = {123};
             int expected = 123321;
 
             stringUtil su = new stringUtil();
             //Act
-            int result = su.nextPalindrome(num);
+            int result = su.nextPalindrome(str);
 
             //Assert
-            Assert.AreEqual(expected, result);
+            Assert.Equals(expected, result);
+
+
 
         }
 
@@ -89,32 +67,20 @@ namespace UnitTestProject1
         public void TestNextPrime()
         {
             //Arrange
-            int num =  2 ;
-            int[] expected = { 3, 5, 7 };
+            int[] str = {2};
+            int [] expected = { 3, 5, 7 };
 
-            stringUtil su = new stringUtil();
+            stringUtil su = new  stringUtil();
 
-            //Act
-            int[] result = su.nextPrime(num);
-
-            //Assert
-            CollectionAssert.AreEqual(expected, result);
-
-        }
-
-        [TestMethod]
-        public void TestIsPrime()
-        {
-            int num = 101;
-            bool expected = true;
-
-            stringUtil su = new stringUtil();
 
             //Act
-            bool result = su.isPrime(num);
+
+            int result = su.nextPrime(str);
 
             //Assert
-            Assert.AreEqual(expected, result);
+
+            Assert.Equals(expected, result);
+
         }
     }
 }
